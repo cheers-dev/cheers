cc4b079edf3d70aed905764223d6f9cdeca3ed6a
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-19 03:23:49 +0800

    feat(ChatsView): create basic chats view frame

---
+++ Cheers/Views/Chats/ChatsView.swift
@@ -0,0 +1,44 @@
+//
+//  ChatsView.swift
+//  cheers
+//
+//  Created by Dong on 2024/3/19.
+//
+
+import SwiftUI
+
+struct ChatsView: View {
+    @State var search = ""
+    var body: some View {
+        NavigationStack {
+            List {
+                NavigationLink {
+                    Text("chatting room page")
+                } label: {
+                    ChatCardView(
+                        imageURL: URL(string: ""),
+                        name: "Name",
+                        lastMessage: "testing message",
+                        time: Date()
+                    )
+                    .padding(.trailing)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("聊天室")
+            .toolbarBackground(Color(UIColor.systemGray6), for: .navigationBar)
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "ellipsis.message.fill")
+                        .foregroundStyle(.black)
+                }
+            }
+        }
+        .searchable(text: $search)
+        .navigationBarTitleDisplayMode(.large)
+    }
+}
+
+#Preview {
+    ChatsView()
+}
