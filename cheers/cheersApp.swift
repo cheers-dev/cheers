d2f81d7a50620060d89b9860f30bbb0308128f6e
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-18 23:12:55 +0800

    Initial Commit

---
+++ cheers/cheersApp.swift
@@ -0,0 +1,17 @@
+//
+//  cheersApp.swift
+//  cheers
+//
+//  Created by Dong on 2024/3/18.
+//
+
+import SwiftUI
+
+@main
+struct cheersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
