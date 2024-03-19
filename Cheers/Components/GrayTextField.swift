1344dafd76f1555cc61b1ef868bfefaff1dcb64b
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-20 01:34:23 +0800

    feat(GrayTextField): create customed style text field

---
+++ Cheers/Components/GrayTextField.swift
@@ -0,0 +1,50 @@
+//
+//  GrayTextField.swift
+//  Cheers
+//
+//  Created by Dong on 2024/3/19.
+//
+
+import SwiftUI
+
+struct GrayTextField: View {
+    let type: FieldType
+    let label: String
+    var text: Binding<String>
+    
+    init(_ label: String, text: Binding<String>) {
+        self.label = label
+        self.text = text
+        self.type = .text
+    }
+    
+    init(_ label: String, type: FieldType, text: Binding<String>) {
+        self.label = label
+        self.type = type
+        self.text = text
+    }
+    
+    var body: some View {
+        switch type {
+            case .text:
+                TextField(label, text: text)
+                    .padding(12)
+                    .background(Color(UIColor.systemGray6))
+                    .cornerRadius(12)
+            case .password:
+                SecureField(label, text: text)
+                    .padding(12)
+                    .background(Color(UIColor.systemGray6))
+                    .cornerRadius(12)
+        }   
+    }
+    
+    enum FieldType {
+        case text, password
+    }
+}
+
+#Preview {
+    @State var text = ""
+    return GrayTextField("test", text: $text)
+}
