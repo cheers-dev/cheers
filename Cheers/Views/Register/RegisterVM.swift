431fc4386f7a60181421e08d8aa300fb50ad4eee
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-04-05 04:17:13 +0800

    feat(Register): create and implement register view model

---
+++ Cheers/Views/Register/RegisterVM.swift
@@ -0,0 +1,64 @@
+//
+//  RegisterVM.swift
+//  Cheers
+//
+//  Created by Dong on 2024/3/19.
+//
+
+import Foundation
+
+final class RegisterVM: ObservableObject {
+    @Published var account = ""
+    @Published var password = ""
+    @Published var mail = ""
+    @Published var name = ""
+    @Published var birth = Date()
+    @Published var confirmPassword = ""
+    @Published var error: Error?
+    
+    func submit() {
+//        Task {
+//            try await register()
+//        }
+        print(hashPassword(password))
+    }
+    
+    func register() async throws {
+        do {
+            guard password == confirmPassword
+            else { throw APIError.custom("Password validation failed.") }
+            
+            guard let registerURLText = Bundle.main.infoDictionary?["REGISTER_URL"] as? String,
+                  let registerURL = URL(string: registerURLText.replacing("\\", with: ""))
+            else { throw APIError.invalidURL }
+            
+            print(registerURL)
+            
+            var request = URLRequest(url: registerURL)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let createUser = User.Create(
+                account: account,
+                password: hashPassword(password),
+                mail: mail,
+                name: name,
+                birth: birth.ISO8601Format(.iso8601Date(timeZone: .gmt))
+            )
+            
+            let encoder = JSONEncoder()
+            let jsonData = try encoder.encode(createUser)
+            request.httpBody = jsonData
+            
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  200...299 ~= httpResponse.statusCode
+            else { throw APIError.responseError }
+            
+            print(String(data: data, encoding: .utf8) ?? "")
+        } catch {
+            self.error = error
+        }
+    }
+}
--- Cheers/Views/Register/RegisterView.swift
+++ Cheers/Views/Register/RegisterView.swift
@@ -8,13 +8,11 @@
 import SwiftUI
 
 struct RegisterView: View {
-    @State var mail: String = ""
-    @State var name: String = ""
-    @State var nickname: String = ""
-    @State var password: String = ""
-    @State var birth: Date = Date()
+    @ObservedObject var register: RegisterVM
+    @State var showErrorAlert = false
+
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(spacing: 40) {
             VStack {
                 Image("icon")
                     .resizable()
@@ -23,20 +21,20 @@ struct RegisterView: View {
                     .font(.title3)
                     .fontWeight(.semibold)
             }
-            VStack(spacing: 20) {
-                GrayTextField("信箱", text: $mail)
-                GrayTextField("全名", text: $name)
-                GrayTextField("用戶名稱", text: $nickname)
-                GrayTextField("密碼", text: $password)
-                GrayTextField("密碼", text: $password)
+            VStack(spacing: 12) {
+                GrayTextField("信箱", text: $register.mail)
+                GrayTextField("帳號", text: $register.account)
+                GrayTextField("全名", text: $register.name)
+                GrayTextField("密碼", type: .password, text: $register.password)
+                GrayTextField("再次輸入密碼", type: .password, text: $register.confirmPassword)
                 DatePicker(
                     "生日",
-                    selection: $birth,
+                    selection: $register.birth,
                     displayedComponents: .date
                 )
                 .padding(.leading, 12)
                 .foregroundStyle(.gray)
-                Button(action: {}) {
+                Button(action: { register.submit() }) {
                     Text("註冊")
                         .padding(12)
                         .foregroundStyle(.white)
@@ -54,11 +52,24 @@ struct RegisterView: View {
                 }
             }
         }
+        .scrollDismissesKeyboard(.automatic)
         .font(.system(size: 14))
         .padding()
+        .onReceive(register.$error){ error in
+            if error != nil {
+                showErrorAlert.toggle()
+            }
+        }
+        .alert("Error", isPresented: $showErrorAlert) {
+            Button(action: { showErrorAlert.toggle() }) {
+                Text("OK")
+            }
+        } message: {
+            Text(register.error?.localizedDescription ?? "")
+        }
     }
 }
 
 #Preview {
-    RegisterView()
+    RegisterView(register: RegisterVM())
 }
