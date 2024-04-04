7080ec637ea79f2b0578b174b84db70bc0b2d866
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-04-05 03:55:15 +0800

    feat(FormDataReauest): create form-data request with data upload func

--- Cheers/Extensions/NSMutableData.swift
+++ Cheers/Extensions/NSMutableData.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension NSMutableData {
+    func append(_ string: String) {
+        if let data = string.data(using: .utf8) {
+            self.append(data)
+        }
+    }
+}
--- Cheers/Models/MultipartFormDataRequest.swift
+++ Cheers/Models/MultipartFormDataRequest.swift
@@ -6,3 +6,58 @@
 //
 
 import Foundation
+
+struct MultipartFormDataRequest {
+    private let boundary = UUID().uuidString.lowercased()
+    private var httpBody = NSMutableData()
+    
+    let URL: URL
+    
+    init(URL: URL) {
+        self.URL = URL
+    }
+    
+    func addTextField(_ name: String, value: String) {
+        httpBody.append(textField(name, value: value))
+    }
+    
+    func addDataField(_ name: String, data: Data) {
+        httpBody.append(dataField(name, data: data))
+    }
+    
+    func createURLRequest() -> URLRequest {
+        var request = URLRequest(url: URL)
+        
+        request.httpMethod = "POST"
+        request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        
+        httpBody.append("--\(boundary)--")
+        request.httpBody = httpBody as Data
+        return request
+    }
+    
+    
+    private func textField(_ name: String, value: String) -> String {
+        var fieldString = "--\(boundary)\r\n"
+        fieldString += "Content-Disposition: form-data; name=\"\(name)\"\r\n"
+        fieldString += "Content-Type: text/plain\r\n"
+        fieldString += "Content-Transfer-Encoding: 8bit\r\n"
+        fieldString += "\r\n\(value)\r\n"
+        
+        return fieldString
+    }
+    
+    private func dataField(_ name: String, data: Data) -> Data {
+        let fieldData = NSMutableData()
+        
+        fieldData.append("""
+            --\(boundary)\r\n
+            Content-Disposition: form-data; name=\"\(name)\"\r\n
+            Content-Type: \"content-type header\"\r\n\r\n
+        """)
+        fieldData.append(data)
+        fieldData.append("\r\n")
+        
+        return fieldData as Data
+    }
+}
