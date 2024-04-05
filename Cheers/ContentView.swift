26424b68ebd4422f1994ddaa0403d036bd1e7cdf
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-04-06 06:10:30 +0800

    feat(ContentView): add accessToken found handler

--- Cheers/ContentView.swift
+++ Cheers/ContentView.swift
@@ -8,26 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("accessTokenFound") var accessTokenFound = KeychainManager.getToken("accessToken") != nil
+    
     var body: some View {
-        TabView {
-            FriendsListView()
-                .tabItem {
-                    Label("朋友", systemImage: "person.2.fill")
+        Group {
+            if accessTokenFound {
+                TabView {
+                    FriendsListView()
+                        .tabItem {
+                            Label("朋友", systemImage: "person.2.fill")
+                        }
+                    
+                    ChatsView()
+                        .tabItem {
+                            Label("聊天室", systemImage: "ellipsis.message.fill")
+                        }
+                    
+                    UserProfileView(
+                        avatarURL: URL(string: ""),
+                        name: "User name",
+                        birth: Date(),
+                        recentFavorite: ["燒肉", "火鍋", "漢堡"]
+                    )
+                    .tabItem {
+                        Label("用戶", systemImage: "person.fill")
+                    }
                 }
-            
-            ChatsView()
-                .tabItem {
-                    Label("聊天室", systemImage: "ellipsis.message.fill")
-                }
-            
-            UserProfileView(
-                avatarURL: URL(string: ""),
-                name: "User name",
-                birth: Date(),
-                recentFavorite: ["燒肉", "火鍋", "漢堡"]
-            )
-            .tabItem {
-                Label("用戶", systemImage: "person.fill")
+            } else {
+                LoginView(login: LoginVM())
             }
         }
     }
