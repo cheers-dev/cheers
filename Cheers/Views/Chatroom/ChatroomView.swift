81e63f507fc8c7b5cbb999adce5b514840246566
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-06-18 05:34:24 +0800

    refactor(chatroomView): separate body into small views

--- Cheers/Views/Chatroom/ChatroomView.swift
+++ Cheers/Views/Chatroom/ChatroomView.swift
@@ -19,59 +19,9 @@ struct ChatroomView: View {
     var body: some View {
         NavigationStack {
             VStack(spacing: 0) {
-                HStack {
-                    DismissButton()
-                    Text(chatroomVM.chatroom.name)
-                        .font(.title3)
-                    Spacer()
-                    NavigationLink(destination: ChatroomSettingsView()) {
-                        Image(systemName: "line.horizontal.3")
-                            .font(.title3)
-                            .foregroundStyle(.black)
-                    }
-                }
-                .padding()
-                .fontWeight(.semibold)
-                .background(Color(UIColor.systemGray6), ignoresSafeAreaEdges: .top)
-                
-                ScrollViewReader { scrollView in
-                    ScrollView {
-                        ForEach($chatroomVM.messages, id: \.id) { message in
-                            MessageView(
-                                message: message,
-                                isSender: userId == message.wrappedValue.userId.uuidString
-                            )
-                            .id(message.id)
-                        }
-                    }
-                    .frame(maxHeight: .infinity)
-                    .contentMargins(.vertical, 8)
-                    .onReceive(chatroomVM.messages.publisher) { _ in
-                        withAnimation {
-                            scrollView.scrollTo(chatroomVM.messages.last?.id, anchor: .bottom)
-                        }
-                    }
-                }
-                
-                HStack {
-                    TextField("輸入訊息", text: $message)
-                        .font(.footnote)
-                        .clipShape(.capsule)
-                        .textFieldStyle(.roundedBorder)
-                    
-                    if(!message.isEmpty) {
-                        Button(action: {
-                            chatroomVM.sendMessage(message)
-                            message = ""
-                        }) {
-                            Image(systemName: "arrow.up.circle.fill")
-                                .font(.title)
-                                .foregroundStyle(.black)
-                        }
-                    }
-                }
-                .padding()
-                .background(Color(UIColor.systemGray6))
+                header
+                chatMessages
+                footer
             }
             .navigationBarBackButtonHidden()
             .scrollDismissesKeyboard(.interactively)
@@ -91,6 +41,67 @@ struct ChatroomView: View {
                 }
             }
         }
+    }
+    
+    var header: some View {
+        HStack {
+            DismissButton()
+            Text(chatroomVM.chatroom.name)
+                .font(.title3)
+            Spacer()
+            NavigationLink(destination: ChatroomSettingsView()) {
+                Image(systemName: "line.horizontal.3")
+                    .font(.title3)
+                    .foregroundStyle(.black)
+            }
+        }
+        .padding()
+        .fontWeight(.semibold)
+        .background(Color(UIColor.systemGray6), ignoresSafeAreaEdges: .top)
+    }
+    
+    var chatMessages: some View {
+        ScrollViewReader { scrollView in
+            ScrollView {
+                ForEach($chatroomVM.messages, id: \.id) { message in
+                    MessageView(
+                        message: message,
+                        isSender: userId == message.wrappedValue.userId.uuidString
+                    )
+                    .id(message.id)
+                }
+            }
+            .frame(maxHeight: .infinity)
+            .contentMargins(.vertical, 8)
+            .onReceive(chatroomVM.messages.publisher) { _ in
+                withAnimation {
+                    scrollView.scrollTo(chatroomVM.messages.last?.id, anchor: .bottom)
+                }
+            }
+        }
+    }
+    
+    var footer: some View {
+        HStack {
+            TextField("輸入訊息", text: $message)
+                .font(.footnote)
+                .clipShape(.capsule)
+                .textFieldStyle(.roundedBorder)
+            
+            if(!message.isEmpty) {
+                Button(action: {
+                    chatroomVM.sendMessage(message)
+                    message = ""
+                }) {
+                    Image(systemName: "arrow.up.circle.fill")
+                        .font(.title)
+                        .foregroundStyle(.black)
+                }
+            }
+        }
+        .padding()
+        .background(Color(UIColor.systemGray6))
+
     }
 }
 
