ecbf62e59f46898c46fe11bf850ea06b4fdabfaf
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-06-18 05:17:22 +0800

    feat(MessageView): create message bubble component

---
+++ Cheers/Views/Chatroom/MessageView.swift
@@ -0,0 +1,36 @@
+//
+//  MessageView.swift
+//  cheers
+//
+//  Created by Dong on 2024/5/28.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    @Binding var message: Message
+    @State var isSender: Bool
+    
+    var body: some View {
+        HStack {
+            if isSender { Spacer() }
+            
+            Text(message.content)
+                .padding(.vertical, 8)
+                .padding(.horizontal, 12)
+                .foregroundStyle(isSender ? .white : .primary)
+                .background(isSender ? .blue : .init(uiColor: .systemGray5))
+                .clipShape(Capsule())
+            
+            if !isSender { Spacer() }
+        }
+        .padding(.horizontal, 12)
+        .frame(maxWidth: .infinity)
+    }
+}
+
+@available(iOS 18, *)
+#Preview {
+    @Previewable @State var message = Message(id: UUID(), userId: UUID(), content: "Hello, World!", createdAt: nil)
+    MessageView(message: $message, isSender: true)
+}
