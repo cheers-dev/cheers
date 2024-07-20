d8804384076cce4ef97798129d01765595f96848
dong <93483694+dongdong867@users.noreply.github.com>
2024-07-21 04:21:42 +0800

    fix(FriendInvitesView): change title text and move qrcode out of this page

--- Cheers/Views/FriendsList/AddFriend/FriendInvitesView.swift
+++ Cheers/Views/FriendsList/AddFriend/FriendInvitesView.swift
@@ -16,7 +16,7 @@ struct FriendInvitesView: View {
         NavigationStack {
             HStack {
                 DismissButton()
-                Text("添加好友")
+                Text("好友申請")
                 Spacer()
             }
             .padding()
@@ -34,38 +34,6 @@ struct FriendInvitesView: View {
             }
             .padding()
             
-            Spacer()
-            
-            VStack{
-                AsyncImage(url: URL(string: "")) { phase in
-                    switch phase {
-                        case .success(let image):
-                            image
-                                .resizable()
-                        default:
-                            Rectangle()
-                                .fill(Color(UIColor.systemGray5))
-                                .overlay {
-                                    Image(systemName: "exclamationmark.triangle.fill")
-                                        .font(.title)
-                                        .foregroundStyle(.gray)
-                                }
-                    }
-                }
-                .scaledToFill()
-                .background(Color(UIColor.systemGray5))
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                
-                Text("@user_id")
-                    .fontWeight(.semibold)
-                    .padding(8)
-                    .frame(maxWidth: .infinity)
-                    .background(Color(UIColor.systemGray5))
-                    .clipShape(RoundedRectangle(cornerRadius: 8))
-            }
-            .frame(width: 200, height: 200)
-            
-            Spacer()
             Spacer()
         }
         .toolbar(.hidden, for: .tabBar)
