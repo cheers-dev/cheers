278d91750733cb69f8e75464762185684efb4485
dong <93483694+dongdong867@users.noreply.github.com>
2024-07-22 05:08:03 +0800

    feat(RequestWithAccessToken): add reusable send func to handle URLRequest

--- Cheers/Utilities/RequestWithAccessToken.swift
+++ Cheers/Utilities/RequestWithAccessToken.swift
@@ -9,5 +9,37 @@ import Foundation
 
 struct RequestWithAccessToken {
     
+    static func send(_ route: String, methodType: Method) async throws -> Data{
+        guard let accessToken = KeychainManager.getToken("accessToken")
+        else { throw KeychainError.itemNotFound }
+        
+        guard let endpointURLText = Bundle.main.infoDictionary?["GATEWAY_URL"] as? String,
+              let requestURL = URL(string: endpointURLText.replacing("\\", with: "") + route)
+        else { throw APIError.invalidURL }
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = methodType.rawValue
+        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              200...299 ~= httpResponse.statusCode
+        else { throw APIError.responseError }
+        
+        return data
+    }
     
+    static func send<T: Decodable>(_ route: String, methodType: Method) async throws -> T {
+        let data = try await send(route, methodType: methodType)
+        
+        guard let responseJSON = try? JSONDecoder().decode(T.self, from: data)
+        else { throw APIError.invalidData }
+        
+        return responseJSON
+    }
+    
+    enum Method: String {
+        case GET, POST, PUT, DELETE, PATCH
+    }
 }
