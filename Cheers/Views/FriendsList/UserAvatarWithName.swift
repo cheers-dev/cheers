9884f633baa0674223c7a1226bc686702f2e17e2
dong <93483694+dongdong867@users.noreply.github.com>
2024-07-28 01:16:04 +0800

    fix(UserAvatarWithName): remove navigation link from component

--- Cheers/Views/FriendsList/UserAvatarWithName.swift
+++ Cheers/Views/FriendsList/UserAvatarWithName.swift
@@ -12,16 +12,14 @@ struct UserAvatarWithName: View {
     @Binding var user: User
     
     var body: some View {
-        NavigationLink(destination: Text("user detail page")) {
-            HStack(spacing: 12) {
-                AsyncImageWithDefaultImage(imageURL: user.avatar)
-                    .frame(width: 50, height: 50)
-                    .clipShape(Circle())
-                
-                Text(user.name)
-                    .fontWeight(.medium)
-                
-            }
+        HStack(spacing: 12) {
+            AsyncImageWithDefaultImage(imageURL: user.avatar)
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+            
+            Text(user.name)
+                .fontWeight(.medium)
+            
         }
     }
 }
