60dba4fbd074232ca2ee8bf8f798ff94fbdc8807
dong <93483694+dongdong867@users.noreply.github.com>
2024-07-28 02:13:47 +0800

    refactor(FriendsListVM): refactor with RequestWithAccessToken

--- Cheers/Views/FriendsList/FriendsListVM.swift
+++ Cheers/Views/FriendsList/FriendsListVM.swift
@@ -22,28 +22,10 @@ final class FriendsListVM: ObservableObject {
     @MainActor
     func fetchFriendList() async {
         do {
-            guard let accessToken = KeychainManager.getToken("accessToken")
-            else { throw KeychainError.itemNotFound }
-            
-            guard let endpointURLText = Bundle.main.infoDictionary?["GATEWAY_URL"] as? String,
-                  let getFriendListURL = URL(string: endpointURLText.replacing("\\", with: "") + "/friend/getFriends")
-            else { throw APIError.invalidURL }
-            
-            var request = URLRequest(url: getFriendListURL)
-            request.httpMethod = "GET"
-            request.setValue(accessToken, forHTTPHeaderField: "Authorization")
-            
-            let (data, response) = try await URLSession.shared.data(for: request)
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                  200...299 ~= httpResponse.statusCode
-            else { throw APIError.responseError }
-            
-            guard let friends = try? JSONDecoder().decode([User].self, from: data)
-            else { throw APIError.invalidData }
+            let friends: [User] = try await RequestWithAccessToken
+                .send("friend/getFriends", methodType: .GET)
             
             self.friends = friends
-            
         } catch {
             self.error = error
         }
