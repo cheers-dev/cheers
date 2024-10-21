0ca4fdb757db5053a4468ee5d63511c8e9395fed
dong <93483694+dongdong867@users.noreply.github.com>
2024-10-22 05:48:53 +0800

    feat: add request body handler to RequestWithAccessToken

--- Cheers/Utilities/RequestWithAccessToken.swift
+++ Cheers/Utilities/RequestWithAccessToken.swift
@@ -7,9 +7,8 @@
 
 import Foundation
 
-struct RequestWithAccessToken {
-    
-    static func send(_ route: String, methodType: Method) async throws -> Data{
+enum RequestWithAccessToken {
+    static func send(_ route: String, methodType: Method, data: Data? = nil) async throws -> Data {
         guard let accessToken = KeychainManager.getToken("accessToken")
         else { throw KeychainError.itemNotFound }
         
@@ -20,18 +19,21 @@ struct RequestWithAccessToken {
         var request = URLRequest(url: requestURL)
         request.httpMethod = methodType.rawValue
         request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        if let body = data { request.httpBody = body }
         
         let (data, response) = try await URLSession.shared.data(for: request)
         
         guard let httpResponse = response as? HTTPURLResponse,
-              200...299 ~= httpResponse.statusCode
+              200 ... 299 ~= httpResponse.statusCode
         else { throw APIError.responseError }
         
         return data
     }
     
-    static func send<T: Decodable>(_ route: String, methodType: Method) async throws -> T {
-        let data = try await send(route, methodType: methodType)
+    static func send<T: Decodable>(_ route: String, methodType: Method, data: Data? = nil) async throws -> T {
+        let data = try await send(route, methodType: methodType, data: data)
         
         guard let responseJSON = try? JSONDecoder().decode(T.self, from: data)
         else { throw APIError.invalidData }
