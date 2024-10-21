d835b6680941984f63c24e141e4031664172bea8
dong <93483694+dongdong867@users.noreply.github.com>
2024-10-22 05:49:30 +0800

    feat: add chatroom schema for create and display

--- Cheers/Models/Chatroom.swift
+++ Cheers/Models/Chatroom.swift
@@ -7,10 +7,39 @@
 
 import Foundation
 
+// MARK: - Chatroom
+
 struct Chatroom: Identifiable, Codable {
-    
     let id: UUID
     let name: String
     let avatar: URL?
-    
+}
+
+// MARK: Chatroom.Create
+
+extension Chatroom {
+    struct Create: Identifiable, Codable {
+        let id: UUID
+        let name: String
+        let userIds: [UUID]
+        let avatar: URL?
+
+        init(id: UUID, name: String, userIds: [UUID], avatar: URL? = nil) {
+            self.id = id
+            self.name = name
+            self.avatar = avatar
+            self.userIds = userIds
+        }
+    }
+}
+
+// MARK: Chatroom.Info
+
+extension Chatroom {
+    struct Info: Identifiable, Codable {
+        let chatroom: Chatroom
+        let lastMessage: Message?
+
+        var id: UUID { chatroom.id }
+    }
 }
