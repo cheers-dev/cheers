414d81f8382659626966c05f687ad13d6dc22d69
dong <93483694+dongdong867@users.noreply.github.com>
2024-10-22 05:50:01 +0800

    refactor: seperate content view into small components

--- Cheers/ContentView.swift
+++ Cheers/ContentView.swift
@@ -9,33 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     @AppStorage("accessTokenFound") var accessTokenFound = false
-    
+
     var body: some View {
         Group {
-            if accessTokenFound {
-                TabView {
-                    FriendsListView(friendsListVM: FriendsListVM())
-                        .tabItem {
-                            Label("朋友", systemImage: "person.2.fill")
-                        }
-                    
-                    ChatroomListView(chatroomListVM: ChatroomListVM())
-                        .tabItem {
-                            Label("聊天室", systemImage: "ellipsis.message.fill")
-                        }
-                    
-                    UserProfileView(
-                        avatarURL: URL(string: ""),
-                        name: "User name",
-                        birth: Date(),
-                        recentFavorite: ["燒肉", "火鍋", "漢堡"]
-                    )
-                    .tabItem {
-                        Label("用戶", systemImage: "person.fill")
-                    }
+            if accessTokenFound { tabView }
+            else { LoginView(login: LoginVM()) }
+        }
+    }
+
+    var tabView: some View {
+        TabView {
+            FriendsListView(friendsListVM: FriendsListVM())
+                .tabItem {
+                    Label("朋友", systemImage: "person.2.fill")
                 }
-            } else {
-                LoginView(login: LoginVM())
+
+            ChatroomListView(chatroomListVM: ChatroomListVM())
+                .tabItem {
+                    Label("聊天室", systemImage: "ellipsis.message.fill")
+                }
+
+            UserProfileView(
+                avatarURL: URL(string: ""),
+                name: "User name",
+                birth: Date(),
+                recentFavorite: ["燒肉", "火鍋", "漢堡"]
+            )
+            .tabItem {
+                Label("用戶", systemImage: "person.fill")
             }
         }
     }
