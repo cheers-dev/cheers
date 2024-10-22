419b9a4184548bd18e18861c3a94982646953302
dong <93483694+dongdong867@users.noreply.github.com>
2024-10-23 03:53:04 +0800

    feat: create and implement GroupListVM

--- Cheers/Views/FriendsList/GroupsList/GroupsListView.swift
+++ Cheers/Views/FriendsList/GroupsList/GroupsListView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct GroupsListView: View {
+    @ObservedObject var vm: GroupsListVM
+    
     @State var search = ""
+    @State var showErrorAlert = false
     
     var body: some View {
         NavigationStack {
-            List {
-                NavigationLink(destination: ChatroomView()) {
+            List(vm.state.groups) { group in
+                NavigationLink(destination: Text(group.name)) {
                     HStack(spacing: 12) {
-                        AsyncImageWithDefaultImage(imageURL: URL(string: ""))
+                        AsyncImageWithDefaultImage(imageURL: group.avatar ?? URL(string: ""))
                             .frame(width: 50, height: 50)
                             .clipShape(Circle())
-                        Text("Group name")
+                        Text(group.name)
                             .fontWeight(.medium)
-                        
                     }
                 }
             }
@@ -29,19 +31,28 @@ struct GroupsListView: View {
         }
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarBackButtonHidden()
+        .toolbarBackground(.visible, for: .navigationBar)
         .toolbar {
             ToolbarItem(placement: .navigation) {
                 DismissButton()
             }
-            
+
             ToolbarItem(placement: .principal) {
                 Text("群組列表")
             }
         }
-        .toolbarBackground(.visible, for: .navigationBar)
+        .onReceive(vm.$error) { error in
+            if error != nil { showErrorAlert.toggle() }
+        }
+        .alert("Error", isPresented: $showErrorAlert) {
+            Button(action: { showErrorAlert.toggle() }) { Text("OK") }
+        } message: {
+            if vm.error != nil { Text(String(describing: vm.error!)) }
+        }
+
     }
 }
 
 #Preview {
-    GroupsListView()
+    GroupsListView(vm: GroupsListVM())
 }
