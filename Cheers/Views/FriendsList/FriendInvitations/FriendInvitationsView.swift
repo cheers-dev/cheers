59ff02d3338ab56be89c9a6487edea575c487f01
dong <93483694+dongdong867@users.noreply.github.com>
2024-11-03 18:34:49 +0800

    refactor: extract view into subviews

--- Cheers/Views/FriendsList/FriendInvitations/FriendInvitationsView.swift
+++ Cheers/Views/FriendsList/FriendInvitations/FriendInvitationsView.swift
@@ -16,58 +16,9 @@ struct FriendInvitationsView: View {
     
     var body: some View {
         NavigationStack {
-            HStack {
-                DismissButton()
-                Text("好友申請")
-                Spacer()
-            }
-            .padding()
-            
-            .fontWeight(.medium)
-            .background(Color(UIColor.systemGray6), ignoresSafeAreaEdges: .top)
-            
-            HStack {
-                ZStack(alignment: .trailing) {
-                    GrayTextField("輸入使用者 id", text: $searchId)
-                        .font(.footnote)
-                    if (searchId != "") {
-                        Button(action: {}) {
-                            Image(systemName: "magnifyingglass.circle.fill")
-                                .padding(.horizontal, 8)
-                                .font(.title2)
-                                .foregroundStyle(.black)
-                        }
-                    }
-                }
-                Button(action: { isScanSheetShow.toggle() }) {
-                    Image(systemName: "qrcode.viewfinder")
-                        .font(.title)
-                        .foregroundStyle(.black)
-                }
-            }
-            .padding()
-            
-            List($friendInvitationVM.friendInvitations) { $friendInvitation in
-                HStack {
-                    UserAvatarWithName(user: $friendInvitation.requestor)
-                    Spacer()
-                    
-                    Button(action: { Task {
-                        await friendInvitationVM
-                            .acceptInvitation($friendInvitation.wrappedValue.id)
-                    }}) {
-                        Text("接受")
-                    }.tint(.green)
-                    
-                    Button(action: { Task {
-                        await friendInvitationVM
-                            .rejectInvitation($friendInvitation.wrappedValue.id)
-                    }}) {
-                        Text("拒絕")
-                    }.tint(.red)
-                }
-                .buttonStyle(.bordered)
-            }.listStyle(.plain)
+            title
+            userIdInputField
+            invitationsList
         }
         .toolbar(.hidden, for: .tabBar)
         .navigationBarBackButtonHidden()
@@ -86,21 +37,83 @@ struct FriendInvitationsView: View {
             }
         }
         .sheet(isPresented: $isScanSheetShow) {
-            VStack {
-                CodeScannerView(codeTypes: [.qr], simulatedData: "https://dongdong867.dev") { response in
-                    switch response {
-                        case .success(let result):
-                            print(result)
-                        case .failure(let error):
-                            print(error)
-                    }
-                }
-                .padding()
-                .aspectRatio(1, contentMode: .fit)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-            }
+            codeScanner
         }
         .presentationDetents([.medium])
+    }
+    
+    var title: some View {
+        HStack {
+            DismissButton()
+            Text("好友申請")
+            Spacer()
+        }
+        .padding()
+        .fontWeight(.medium)
+        .background(Color(UIColor.systemGray6), ignoresSafeAreaEdges: .top)
+    }
+    
+    var userIdInputField: some View {
+        HStack {
+            ZStack(alignment: .trailing) {
+                GrayTextField("輸入使用者 id", text: $searchId)
+                    .font(.footnote)
+                if searchId != "" {
+                    Button(action: {}) {
+                        Image(systemName: "magnifyingglass.circle.fill")
+                            .padding(.horizontal, 8)
+                            .font(.title2)
+                            .foregroundStyle(.black)
+                    }
+                }
+            }
+            Button(action: {}) {
+                Image(systemName: "qrcode.viewfinder")
+                    .font(.title)
+                    .foregroundStyle(.black)
+            }
+        }
+        .padding()
+    }
+    
+    var invitationsList: some View {
+        List($friendInvitationVM.friendInvitations) { $friendInvitation in
+            HStack {
+                UserAvatarWithName(user: $friendInvitation.requestor)
+                Spacer()
+                
+                Button(action: { Task {
+                    await friendInvitationVM
+                        .acceptInvitation($friendInvitation.wrappedValue.id)
+                }}) {
+                    Text("接受")
+                }.tint(.green)
+                
+                Button(action: { Task {
+                    await friendInvitationVM
+                        .rejectInvitation($friendInvitation.wrappedValue.id)
+                }}) {
+                    Text("拒絕")
+                }.tint(.red)
+            }
+            .buttonStyle(.bordered)
+        }.listStyle(.plain)
+    }
+    
+    var codeScanner: some View {
+        VStack {
+            CodeScannerView(codeTypes: [.qr], simulatedData: "https://dongdong867.dev") { response in
+                switch response {
+                    case .success(let result):
+                        print(result)
+                    case .failure(let error):
+                        print(error)
+                }
+            }
+            .padding()
+            .aspectRatio(1, contentMode: .fit)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+        }
     }
 }
 
