607c8ac353e204217fa7e1d75812e7ad377fe3f1
dong <93483694+dongdong867@users.noreply.github.com>
2024-11-03 17:38:02 +0800

    refactor: make date parse a static string

--- Cheers/Models/Message.swift
+++ Cheers/Models/Message.swift
@@ -7,12 +7,18 @@
 
 import Foundation
 
+// MARK: - Message
+
 struct Message: Identifiable, Codable {
-    
     let id: UUID
     let userId: UUID
     let content: String
     let createdAt: Date?
+    
+    private static let dateFormatter: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        return formatter
+    }()
     
     init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
@@ -20,9 +26,8 @@ struct Message: Identifiable, Codable {
         self.userId = try container.decode(UUID.self, forKey: .userId)
         self.content = try container.decode(String.self, forKey: .content)
         
-        let createdAtString = (try container.decode(String.self, forKey: .createdAt))
-                                .replacingOccurrences(of: "\\.\\d+", with: "", options:.regularExpression)
-        self.createdAt = ISO8601DateFormatter().date(from: createdAtString)
+        let createdAtString = try container.decode(String.self, forKey: .createdAt)
+        self.createdAt = try Message.parseDate(createdAtString)
     }
     
     init(id: UUID, userId: UUID, content: String, createdAt: Date?) {
@@ -31,17 +36,28 @@ struct Message: Identifiable, Codable {
         self.content = content
         self.createdAt = createdAt
     }
+    
+    private static func parseDate(_ dateString: String) throws -> Date {
+        let formattedString = dateString.replacingOccurrences(
+            of: "\\.\\d+",
+            with: "",
+            options: .regularExpression
+        )
+        
+        guard let date = dateFormatter.date(from: formattedString)
+        else { throw APIError.invalidData }
+        
+        return date
+    }
 }
 
-
 extension Message {
-    
     static func dictionaryToMessage(_ dict: [String: String]) -> Message? {
         guard let id = UUID(uuidString: dict["id"] ?? ""),
               let userId = UUID(uuidString: dict["userId"] ?? ""),
               let content = dict["content"],
               let createdAtString = dict["createdAt"]?
-                                        .replacingOccurrences(of: "\\.\\d+", with: "", options:.regularExpression),
+              .replacingOccurrences(of: "\\.\\d+", with: "", options: .regularExpression),
               let createdAt = ISO8601DateFormatter().date(from: createdAtString)
         else { return nil }
         
