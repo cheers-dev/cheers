82a4a80d92373618166c7c424a458dfc4693ebd7
dong <93483694+dongdong867@users.noreply.github.com>
2024-11-03 16:19:18 +0800

    refactor: make api call reusable

--- Cheers/Views/FriendsList/FriendInvitations/FriendInvitationsVM.swift
+++ Cheers/Views/FriendsList/FriendInvitations/FriendInvitationsVM.swift
@@ -11,55 +11,48 @@ final class FriendInvitationsVM: ObservableObject {
     @Published var friendInvitations = [FriendInvitation]()
     @Published var error: Error?
     
-    init() {
-        loadFriendInvitations()
-    }
+    private var loadingTask: Task<Void, Never>?
     
-    private func loadFriendInvitations() {
-        Task {
-            await fetchFriendInvitations()
+    init() {
+        Task { @MainActor in
+            await self.fetchFriendInvitations()
         }
     }
+    
+    deinit { loadingTask?.cancel() }
     
     func acceptInvitation(_ id: UUID) async {
-        do {
-            let _ = try await RequestWithAccessToken.send(
-                "friend/accept?id=\(id)",
-                methodType: .PATCH
-            )
-            
-            self.friendInvitations.removeAll { invitation in
-                invitation.id == id
-            }
-        } catch {
-            self.error = error
-        }
+        await handleInvitation(id, action: "accept")
     }
     
     func rejectInvitation(_ id: UUID) async {
-        do {
-            let _ = try await RequestWithAccessToken.send(
-                "friend/reject?id=\(id)",
-                methodType: .PATCH
-            )
-            
-            self.friendInvitations.removeAll { invitation in
-                invitation.id == id
-            }
-        } catch {
-            self.error = error
-        }
+        await handleInvitation(id, action: "reject")
     }
     
     @MainActor
     private func fetchFriendInvitations() async {
-        do{
+        do {
             let friendInvitations: [FriendInvitation] = try await RequestWithAccessToken
-                .send("/friend/getInvites", methodType: .GET)
+                .send("friend/getInvites", methodType: .GET)
             
             self.friendInvitations = friendInvitations
         } catch {
             self.error = error
+        }
+    }
+    
+    private func handleInvitation(_ id: UUID, action: String) async {
+        do {
+            let _ = try await RequestWithAccessToken
+                .send("friend/\(action)?id=\(id)", methodType: .PATCH)
+            
+            await MainActor.run {
+                self.friendInvitations.removeAll { $0.id == id }
+            }
+        } catch {
+            await MainActor.run {
+                self.error = error
+            }
         }
     }
 }
