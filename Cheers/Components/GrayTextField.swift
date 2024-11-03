64d255760a41cc2e7d52a15cba714a296ceae5af
dong <93483694+dongdong867@users.noreply.github.com>
2024-11-03 15:56:23 +0800

    refactor: seperate reusable code

--- Cheers/Components/GrayTextField.swift
+++ Cheers/Components/GrayTextField.swift
@@ -11,38 +11,38 @@ struct GrayTextField: View {
     let type: FieldType
     let label: String
     var text: Binding<String>
-    
+
     init(_ label: String, text: Binding<String>) {
         self.label = label
         self.text = text
         self.type = .text
     }
-    
+
     init(_ label: String, type: FieldType, text: Binding<String>) {
         self.label = label
         self.type = type
         self.text = text
     }
-    
+
     var body: some View {
         switch type {
             case .text:
-                TextField(label, text: text)
-                    .padding(12)
-                    .background(Color(UIColor.systemGray6))
-                    .cornerRadius(12)
-                    .textInputAutocapitalization(.never)
+                textFieldStyle(TextField(label, text: text))
+
             case .password:
-                SecureField(label, text: text)
-                    .padding(12)
-                    .background(Color(UIColor.systemGray6))
-                    .cornerRadius(12)
-                    .textInputAutocapitalization(.never)
+                textFieldStyle(SecureField(label, text: text))
         }
     }
-    
+
     enum FieldType {
         case text, password
+    }
+
+    private func textFieldStyle<T: View>(_ view: T) -> some View {
+        view.padding(12)
+            .background(Color(UIColor.systemGray6))
+            .cornerRadius(12)
+            .textInputAutocapitalization(.never)
     }
 }
 
